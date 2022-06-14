8acc256e5cadd32be98d696d2ddb09dc3dec8ebd
Karl-Gustav Kallasmaa <karl-gustav.kallasmaa@transferwise.com>
2022-06-14 20:33:18 +0100

    add cart

--- sphere/item/order_line_item.py
+++ sphere/item/order_line_item.py
@@ -1,10 +1,11 @@
-from typing import Optional, List, Dict
-
+from decimal import Decimal
+from typing import Optional, List, Dict, Union
+import copy
 from bson import ObjectId
 from pydantic import Field, BaseModel
 
 from finance.currency import Currency
-from finance.money import Money, money_sum
+from finance.money import Money, money_sum, money_multiply
 from item.order_line_item_applied_discount import OrderLineItemAppliedDiscount
 from item.order_line_item_applied_tax import OrderLineItemAppliedTax
 from item.order_quantity_unit import OrderQuantityUnit
@@ -102,17 +103,57 @@ class OrderLineItem(BaseModel):
             total -= self.totalDiscountMoney.amount
         return Money(amount=total, currency=currency)
 
+    # TODO: unit test
+    def quantity_updated(self, new_quantity: OrderQuantityUnit):
+        if new_quantity.quantity.is_zero():
+            self.grossSalesMoney = Money(currency=self.basePriceMoney.currency)
+            self.totalTaxMoney = None
+            self.totalDiscountMoney = None
+            self.totalMoney = Money(currency=self.basePriceMoney.currency)
+            return None
+        multiplier = new_quantity.quantity / self.quantityUnit.quantity
+        item_currency = self.basePriceMoney.currency
 
-def calculate_total_money(currency: Currency, item: Dict[str, any]) -> Money:
-    discount_money = item.get("totalDiscountMoney", None)
-    if discount_money is not None:
-        discount_money = Money(**discount_money)
-        discount_money.amount = -discount_money.amount
-    tax_money = item.get("totalTaxMoney", None)
-    if tax_money is not None:
-        tax_money = Money(**tax_money)
-    gross_sales_money = item.get("grossSalesMoney", None)
-    if gross_sales_money is not None:
-        gross_sales_money = Money(**gross_sales_money)
-    calculations = [gross_sales_money, tax_money, discount_money]
-    return money_sum(calculations).get(currency)
+        self.quantityUnit = new_quantity
+        self.grossSalesMoney = Money(amount=money_multiply(new_quantity, self.basePriceMoney).amount,
+                                     currency=self.grossSalesMoney.currency)
+        self.appliedTaxes = self.__apply_multiplier(multiplier, self.appliedTaxes)
+        self.appliedDiscounts = self.__apply_multiplier(multiplier, self.appliedDiscounts)
+
+        tax_money = []
+        if self.appliedTaxes is not None:
+            tax_money = [t.money for t in self.appliedTaxes]
+        self.totalTaxMoney = money_sum(tax_money).get(item_currency)
+
+        discount_money = []
+        if self.appliedDiscounts is not None:
+            discount_money = [t.money for t in self.appliedDiscounts]
+        self.totalDiscountMoney = money_sum(discount_money).get(item_currency)
+        self.totalMoney = self.__calculate_total_money(item_currency, copy.deepcopy(self.dict()))
+
+    @staticmethod
+    def __apply_multiplier(multiplier: Decimal, elements: Union[
+        Optional[List[OrderLineItemAppliedTax]], Optional[List[OrderLineItemAppliedDiscount]]]) -> Union[
+        Optional[List[OrderLineItemAppliedTax]], Optional[List[OrderLineItemAppliedDiscount]]]:
+        if elements is not None and len(elements) > 0:
+            modified_values = []
+            multiplier_quantity = OrderQuantityUnit(quantity=multiplier, precision=3)
+            for s in elements:
+                s.money = money_multiply(multiplier_quantity, s.money)
+                modified_values.append(s)
+            return modified_values
+
+    @staticmethod
+    def __calculate_total_money(currency: Currency, item: Dict[str, any]) -> Money:
+        discount_money = item.get("totalDiscountMoney", None)
+        if discount_money is not None:
+            discount_money = Money(**discount_money)
+            discount_money.amount = -discount_money.amount
+        tax_money = item.get("totalTaxMoney", None)
+        if tax_money is not None:
+            tax_money = Money(**tax_money)
+        gross_sales_money = item.get("grossSalesMoney", None)
+        if gross_sales_money is not None:
+            gross_sales_money = Money(**gross_sales_money)
+        calculations = [gross_sales_money, tax_money, discount_money]
+        return money_sum(calculations).get(currency)
