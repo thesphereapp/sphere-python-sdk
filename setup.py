cee608d59b2c0ef5d56ad485bf62556f71fa940e
Karl-Gustav Kallasmaa <karl-gustav.kallasmaa@transferwise.com>
2022-06-14 20:09:34 +0100

    renaming

--- setup.py
+++ setup.py
@@ -4,15 +4,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='sphere-interface',
+    name='sphere-python-sdk',
     version='0.0.1',
-    author='Sphere ',
+    author='Sphere developers',
     author_email='hello@thesphereapp.com',
     description='Sharing sphere functionality with the world',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/thesphereapp/sphere-interface',
+    url='https://github.com/thesphereapp/sphere-python-sdk',
     license='MIT',
-    packages=['sphere-interface'],
-    install_requires=['pydantic', 'python-dateutil'],
+    packages=['sphere-api'],
+    install_requires=[
+        'pydantic',
+        'python-dateutil',
+        'pymongo',
+        'setuptools'
+    ],
 )
